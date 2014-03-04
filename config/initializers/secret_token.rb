20a70e8f2eb0649f7d73ccf9640952c3c65aa9a1
Skaradams <skaradams@ubuntu.(none)>
2014-03-04 14:28:08 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keycoopt::Application.config.secret_token = '5c192afdb08e13680e7ebc61bbb98b551b214d9e81a4a7887c3cc2e898e6b49bf3cd6cfa65d6891fd47b2a1c2ded6aebd092db76bfd329b74ac080b647fdd6aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Keycoopt::Application.config.session_store :cookie_store, key: '_keycoopt_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Keycoopt::Application.config.session_store :active_record_store
